a3877736f5f0832efac36ca5c55a29b74a07ada4
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-07-28 09:51:46 -0400

    added copyright info to selected files

--- lion_core/graph/graph.py
+++ lion_core/graph/graph.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2024 HaiyangLi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from typing import Any, Literal
 
 from pydantic import Field
@@ -58,9 +74,8 @@ class Graph(Node):
     ):
         try:
             for i in [head, tail]:
-                if i not in self:
-                    if not isinstance(i, Relational):
-                        raise LionRelationError(f"Node {i} not found in the graph.")
+                if i not in self and not isinstance(i, Relational):
+                    raise LionRelationError(f"Node {i} not found in the graph.")
 
             # if head and tail are not in self.internal_nodes:
             # they are relational objects now after the above check
