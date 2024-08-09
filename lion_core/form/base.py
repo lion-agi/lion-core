b3b448b3747b40597e45224afa01461c976599b4
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-08-09 19:22:49 -0400

    updated

--- lion_core/form/base.py
+++ lion_core/form/base.py
@@ -126,27 +126,10 @@ class BaseForm(Component, MutableRecord):
             result = {k: v for k, v in result.items() if v not in invalid_values}
         return result
 
-    def display(
-        self,
-        fields=None,
-    ):
-        """
-        Displays the form fields using IPython display.
+    @property
+    def display_dict(self) -> dict[str, Any]:
+        return self.required_dict
 
-        Args:
-            fields (optional): Specific fields to display. Defaults to None.
-        """
-        from IPython.display import display, Markdown
-
-        fields = fields or self.required_dict
-
-        for k, v in fields.items():
-            if isinstance(v, dict):
-                v = as_readable_json(v)
-            if len(str(v)) > 50:
-                display(Markdown(f"**{k}**: \n {v}"))
-            else:
-                display(Markdown(f"**{k}**: {v}"))
 
 
 __all__ = ["BaseForm", "BaseTaskForm"]
