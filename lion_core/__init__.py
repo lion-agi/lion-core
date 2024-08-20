eee203113beba26f57ec4cf0e3a3e2d9e44f4c64
OceanLi <quantocean.li@gmail.com>
2024-08-20 17:46:34 +0000

    updated init

--- lion_core/__init__.py
+++ lion_core/__init__.py
@@ -2,7 +2,18 @@
 
 import logging
 
+from .log_manager import event_log_manager, global_log_manager
+from .setting import BASE_LION_FIELDS, LION_ID_CONFIG, LN_UNDEFINED
 from .version import __version__
+
+__all__ = [
+    "log_manager",
+    "LN_UNDEFINED",
+    "LION_ID_CONFIG",
+    "BASE_LION_FIELDS",
+    "event_log_manager",
+    "global_log_manager",
+]
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
