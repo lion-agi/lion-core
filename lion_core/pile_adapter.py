61d805c7bcef272298fe7422a30d82e194c78cad
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-10-04 11:18:21 -0400

    updated `pile_adapter` functional now

--- lion_core/pile_adapter.py
+++ lion_core/pile_adapter.py
@@ -1,8 +1,7 @@
 import json
 from pathlib import Path
-from typing import Any, Literal, Protocol, TypeVar, runtime_checkable
+from typing import Any, Protocol, TypeVar, runtime_checkable
 
-from lionabc import Collective
 from lionfuncs import save_to_file, to_dict
 
 T = TypeVar("T")
@@ -37,10 +36,6 @@ class JsonDumper(Dumper):
         cls,
         subj: T,
         /,
-        *,
-        directory: Path | str = None,
-        filename: str = None,
-        timestamp: bool = True,
         **kwargs,
     ):
         """
@@ -49,9 +44,6 @@ class JsonDumper(Dumper):
         str_ = json.dumps(subj.to_dict())
         save_to_file(
             str_,
-            directory=directory or cls.default_directory,
-            filename=filename or cls.default_filename,
-            timestamp=timestamp,
             extension=cls.obj_key,
             **kwargs,
         )
@@ -66,9 +58,7 @@ class Loader(Protocol):
     @classmethod
     def load_from(
         cls,
-        subj_cls: type[Collective],
         obj: Any,
-        /,
         **kwargs: Any,
     ):
         raise NotImplementedError
@@ -81,10 +71,7 @@ class JsonLoader(Loader):
     @classmethod
     def load_from(
         cls,
-        subj_cls: type[Collective],
         obj: str,
-        /,
-        *,
         recursive: bool = False,
         recursive_python_only: bool = True,
         **kwargs: Any,
@@ -104,27 +91,33 @@ class AdapterRegistry:
     _dumpers: dict[str, Dumper] = {}
 
     @classmethod
-    def register(cls, adapter: type[Dumper] | type[Loader], /) -> None:
-        err_msg = ""
-
-        if not isinstance(adapter, type(Dumper)) and not isinstance(
-            adapter, type(Loader)
-        ):
-            err_msg += (
-                "In order to register the adapter, it needs to be a "
-                "subclass of the <Dumper> or <Loader> protocol. "
-            )
-            if isinstance(adapter, Dumper) or isinstance(adapter, Loader):
-                err_msg += (
-                    f"The converter value received <{adapter.obj_key}> "
-                    "is an instance. Did you mean to register the class?"
+    def register(cls, adapter: Dumper | Loader, /) -> None:
+        if isinstance(adapter, Dumper):
+            if not hasattr(adapter, "dump_to"):
+                raise ValueError(
+                    "The <Dumper> protocol requires the method "
+                    "<dump_to> to be implemented. "
                 )
-            raise ValueError(err_msg)
-
-        if isinstance(adapter, type(Loader)):
-            cls._loaders[adapter.obj_key] = adapter()
+            cls._dumpers[adapter.obj_key] = adapter
+        elif isinstance(adapter, Loader):
+            if not hasattr(adapter, "load_from"):
+                raise ValueError(
+                    "The <Loader> protocol requires the method "
+                    "<load_from> to be implemented. "
+                )
+            cls._loaders[adapter.obj_key] = adapter
         else:
-            cls._dumpers[adapter.obj_key] = adapter()
+            raise ValueError(
+                "The adapter needs to be a subclass of the <Dumper> or"
+                "<Loader> protocol."
+            )
+
+    @classmethod
+    def all_keys(cls) -> dict[str, list[str]]:
+        return {
+            "loader": cls.list_loader_keys(),
+            "dumper": cls.list_dumper_keys(),
+        }
 
     @classmethod
     def list_loader_keys(cls) -> list[str]:
@@ -135,42 +128,9 @@ class AdapterRegistry:
         return list(cls._dumpers.keys())
 
     @classmethod
-    def get(
-        cls, kind_: Literal["load", "dump"], obj_key: str, /
-    ) -> Loader | Dumper:
-        try:
-            match kind_:
-                case "load":
-                    return cls._loaders[obj_key]
-                case "dump":
-                    return cls._dumpers[obj_key]
-            raise KeyError(
-                f"No {kind_}er found for {obj_key}. "
-                "Check if it is registered."
-            )
-        except Exception as e:
-            raise e
+    def load(cls, obj, obj_key, /, **kwargs) -> dict | list[dict]:
+        return cls._loaders[obj_key].load_from(obj, **kwargs)
 
     @classmethod
-    def load(
-        cls,
-        subj_cls: type[Collective],
-        obj: Any,
-        obj_key: str = None,
-        /,
-        **kwargs,
-    ) -> dict[str, Any]:
-        return cls.get("load", obj_key).load_from(subj_cls, obj, **kwargs)
-
-    @classmethod
-    def dump(
-        cls,
-        subj: Collective,
-        obj_key: str,
-        /,
-        **kwargs,
-    ):
-        return cls.get("dump", obj_key).dump_to(subj, **kwargs)
-
-
-__all__ = ["AdapterRegistry", "Loader", "Dumper"]
+    def dump(cls, subj, obj_key, /, **kwargs):
+        cls._dumpers[obj_key].dump_to(subj, **kwargs)
