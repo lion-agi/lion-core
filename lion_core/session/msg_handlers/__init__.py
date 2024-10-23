ff51b5534788db0f3317898ec36e75486fd45fae
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-10-22 22:11:55 -0600

    removed create message in msg handlers

--- lion_core/session/msg_handlers/__init__.py
+++ lion_core/session/msg_handlers/__init__.py
@@ -2,7 +2,6 @@ from .create_action_request_msg import create_action_request
 from .create_action_response_msg import create_action_response
 from .create_assistant_response_msg import create_assistant_response_message
 from .create_instruction_msg import create_instruction_message
-from .create_message import create_message
 from .create_system_msg import create_system_message
 from .validate_message import validate_message
 
@@ -12,6 +11,5 @@ __all__ = [
     "create_assistant_response_message",
     "create_instruction_message",
     "create_system_message",
-    "create_message",
     "validate_message",
 ]
